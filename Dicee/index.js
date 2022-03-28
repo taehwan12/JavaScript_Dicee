c302b7d50291b36d52939a27d258259d524b8ed4
taehwan12 <99249827+taehwan12@users.noreply.github.com>
2022-03-28 14:34:53 +0900

    Add files via upload

---
+++ Dicee/index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").textContent = "🏳️‍🌈 Player 1 Wins!";
+}
+else if(randomNumber1 == randomNumber2){
+  document.querySelector("h1").textContent = "🏳️ Draw";
+}
+else if(randomNumber1 < randomNumber2){
+  document.querySelector("h1").textContent = "Player 2 Wins!‍ 🏳️‍🌈";
+}
